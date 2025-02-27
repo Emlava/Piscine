f10740207825ed9e3e2029322dda0bfe1b90dfce
emlara-va <emiliolaravargas@hotmail.com>
2025-02-27 22:13:47 +0100

    added 03-04

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elara-va <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 17:06:48 by elara-va          #+#    #+#             */
+/*   Updated: 2025/02/27 20:11:03 by elara-va         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	char	*dest_start;
+
+	dest = malloc(ft_strlen(src) + 1);
+	if (!dest)
+		return (NULL);
+	dest_start = dest;
+	while (*src)
+	{
+		*dest_start = *src;
+		src++;
+		dest_start++;
+	}
+	*dest_start = '\0';
+	return (dest);
+}
+
+struct	t_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*tab;
+	int			i;
+
+	tab = malloc(sizeof(t_stock_str) * ac + 1);
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	tab[i].str = 0;
+	return (tab);
+}
